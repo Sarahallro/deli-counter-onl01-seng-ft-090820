f71bfbc4b8194197fe98d14c0f6a68bdb1ad36c6
Sarah Allrozamo <sarahallrozamo@gmail.com>
2020-09-16 21:34:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,8 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     current_line = "The line is currently:"
-    katz_deli.each_with_index do |customer, counter|
-      current_line << " #{counter +1}. #{customer}"
+    katz_deli.each.with_index(1) do |customer, counter|
+      current_line << " #{counter}. #{customer}"
     end
     puts current_line
   end
